43375764910fbb56e0012743cb1f14f91a41e9b2
sasnigdho <salmanahmedims@gmail.com>
2022-09-02 23:59:27 +0600

    Isar instance created in main method.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_isar_db/models/hadith.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'isar_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationSupportDirectory();
+  final isar = await Isar.open(schemas: [HadithSchema], directory: dir.path);
+
   runApp(const IsarApp());
 }
-
-
